374b726fcd3cd4a2988acbda821f1e833bf2a766
Aqua <aqua@arivell.com>
2026-04-26 22:45:59 +0900

    feat(models): export shared model types

---
+++ mcp_server/models/__init__.py
@@ -0,0 +1,24 @@
+from mcp_server.models.common import CommonToolRequest, CommonToolResult
+from mcp_server.models.domain import (
+    AssetSpec,
+    ExportRecord,
+    OperationLog,
+    PartSpec,
+    QAReport,
+    SceneSpec,
+    SnapshotMetadata,
+    WorldSpec,
+)
+
+__all__ = [
+    "AssetSpec",
+    "CommonToolRequest",
+    "CommonToolResult",
+    "ExportRecord",
+    "OperationLog",
+    "PartSpec",
+    "QAReport",
+    "SceneSpec",
+    "SnapshotMetadata",
+    "WorldSpec",
+]
